589c9367c1749d1666e35c7867f09a477ed00ad9
Maykel Oropeza <maykel.oropeza@gmail.com>
2025-03-26 23:10:27 +0400

    Add user Mock for Testing purposes

---
+++ __test__/mocks/user.mock.ts
@@ -0,0 +1,10 @@
+//Test Purposes 
+export const mockUser = {
+    firstName: "Maykel",
+    lastName: "Oropeza",
+    gender: "male",
+    country: "AE",
+    phoneNumber: "+971585619569",
+    email: "maykel.oropeza@example.com",
+    termsOfService: true
+}
